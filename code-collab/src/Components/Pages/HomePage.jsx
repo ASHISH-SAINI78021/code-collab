f730fa9bde1ea50d45d3f15314cbb89348135bb6
Ashish Saini <ashu78021@gmail.com>
2024-02-28 15:34:45 +0530

    updated layout

--- code-collab/src/Components/Pages/HomePage.jsx
+++ code-collab/src/Components/Pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Layout2 from '../Layout/Layout'
+import Layout2 from '../Layout/Layout2'
 
 
 const HomePage = () => {
   return (
     <Layout2>
-      
+      Hi , I am home page 
     </Layout2>
   )
 }
